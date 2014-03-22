975dc7df78e9785cc4e71d767187ed64df97f309
srcLakeJake <jake.mabee@gmail.com>
2014-03-22 17:27:05 -0400

    added user_basics

--- ddl.sql
+++ ddl.sql
@@ -1,1 +1,11 @@
 create schema if not exists rnt;
+
+create table if not exists user_basics (
+username varchar(20),
+pwd varchar(100),
+last_name varchar(30),
+first_name varchar(20),
+middle_name varchar(20),
+email varchar(40)
+)
+comment = 'Basic info for app users';
