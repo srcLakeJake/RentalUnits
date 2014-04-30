03266ed0915306203fbe21d39316d7ce473d1dab
srcLakeJake <jake.mabee@gmail.com>
2014-04-29 22:31:57 -0400

    Added first successful authentication code

--- lib/RentalUnits/App.pm
+++ lib/RentalUnits/App.pm
@@ -2,12 +2,56 @@ package RentalUnits::App;
 
 use Mojo::Base 'Mojolicious';
 
+use Mojolicious::Plugin::Authentication;
 #use RentalUnits::DB;
 
 sub startup {
   my $self = shift;  
 
   $self->secret('My_h0v3rCr@fT_1$_Fuil_oF_e3l$#');
+  
+  $self->plugin( 'authentication' => {
+      #'autoload_user' => 1,
+	  'session_key' => 'turds',
+	  'load_user' => sub {
+          my ( $app, $uid ) = @_;	
+	      my %users = (
+	          jake => {
+		          group => 'admins',
+			      name  => 'Jake Mabee',
+		      },
+		      maki => {
+		          group => 'basic',
+			      name  => 'Maki Tanigaki',
+		      },
+	      );
+		  my $user;
+	      if ( exists $users{$uid} ) {
+	          $user = \$users{$uid};
+	      }	
+	      return $user;
+      },
+	  'validate_user' => sub {
+          my ( $app, $username, $pwd, $extradata ) = @_;		
+	      my %creds = (
+	          jake => 'canoes05',
+		      maki => 'minipanda',
+	      );	
+	      if ( exists $creds{$username} ) {
+	          if ( $creds{$username} eq $pwd ) {
+		          return $username;
+		  }
+		  else {
+		      return undef;
+		  }
+	  }
+	  else {
+	      return undef;
+	  }	
+	  return undef;
+    },
+	#'current_user_fn' => 'user',
+  });
 
   $self->routes()->get('/all_users')->to(   
     controller => 'User',
@@ -24,6 +68,11 @@ sub startup {
     controller => 'Application',
     action     => 'post_application',
     validation_class => 'RentalUnits::Validation::Application',
+  );
+  
+  $self->routes()->post('/login/:username/:password')->to(
+    controller => 'Login',
+    action     => 'log_in',    
   );
   
   my $auth = $self->routes()->bridge('/auth')->to(cb => sub {
@@ -45,9 +94,75 @@ sub startup {
   $auth->route('/secured_stuff')->to(
       controller => 'Stuff',
 	  action      => 'show_stuff',
-  );
+  );  
 
   return;
+}	
+
+sub log_in {
+    my $self = shift;
+	
+	my $username = $self->param('username');
+	my $password = $self->param('password');	
+  
+  my $auth_msg;
+  if ( $self->authenticate( $username, $password, ) ) {
+      $auth_msg = "Login succeeded!\n";
+  }
+  else {
+      $auth_msg = "Login failed.\n";
+  }
+
+  $self->render( json => { 
+      auth_message => $auth_msg,
+  } );
+  
+  return;
+}
+
+sub load_user {
+    my ( $app, $uid ) = @_;
+	
+	my %users = (
+	    jake => {
+		    group => 'admins',
+			name  => 'Jake Mabee',
+		},
+		maki => {
+		    group => 'basic',
+			name  => 'Maki Tanigaki',
+		},
+	);
+	
+	my $user;
+	if ( exists $users{$uid} ) {
+	    $user = \$users{$uid};
+	}
+	
+	return $user;
+}
+
+sub validate_user {
+    my ( $app, $username, $pwd, $extradata ) = @_;	
+	
+	my %creds = (
+	    jake => 'canoes05',
+		maki => 'minipanda',
+	);
+	
+	if ( exists $creds{$username} ) {
+	    if ( $creds{$username} eq $pwd ) {
+		    return $username;
+		}
+		else {
+		    return undef;
+		}
+	}
+	else {
+	    return undef;
+	}
+	
+	return undef;
 }	
 
 1;
